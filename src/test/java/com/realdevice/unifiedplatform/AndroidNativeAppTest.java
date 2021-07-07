987e4b1d3c6ecce49570224ef58481aa22ef2727
shridhar <shridharkalagi@gmail.com>
2021-07-07 21:41:56 +0530

    first commit

---
+++ src/test/java/com/realdevice/unifiedplatform/AndroidNativeAppTest.java
@@ -0,0 +1,80 @@
+package com.realdevice.unifiedplatform;
+
+import com.realdevice.unifiedplatform.SauceTestWatcher;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.Assert.assertTrue;
+
+public class AndroidNativeAppTest {
+    @Rule
+    public TestName name = new TestName() {
+        public String getMethodName() {
+            return String.format("%s", super.getMethodName());
+        }
+    };
+    //This rule allows us to set test status with Junit
+    @Rule
+    public SauceTestWatcher resultReportingTestWatcher = new SauceTestWatcher();
+    public AppiumDriver<MobileElement> driver;
+
+    public AppiumDriver<MobileElement> getDriver() {
+        return driver;
+    }
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        MutableCapabilities capabilities = new MutableCapabilities();
+        /*
+         * Pick your device
+         * */
+        //Not specifying platformVersion or the exact device is the most likely to
+        //find a device in the cloud
+        capabilities.setCapability("platformName", "android");
+//        capabilities.setCapability("deviceName", "Google*");
+
+        capabilities.setCapability("idleTimeout", "90");
+        capabilities.setCapability("noReset", "true");
+        capabilities.setCapability("newCommandTimeout", "90");
+        capabilities.setCapability("appWaitActivity", "com.swaglabsmobileapp.MainActivity");
+        capabilities.setCapability("name", name.getMethodName());
+        capabilities.setCapability("app",
+                "https://github.com/saucelabs/sample-app-mobile/releases/download/2.7.1/Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
+
+        driver = new AndroidDriver<>(
+                new URL("https://" + System.getenv("SAUCE_USERNAME") + ":" +
+                        System.getenv("SAUCE_ACCESS_KEY") +
+                        "@ondemand.eu-central-1.saucelabs.com:443/wd/hub"),
+                capabilities);
+        //Setting the driver so that we can report results
+        resultReportingTestWatcher.setDriver(driver);
+    }
+
+    @Test
+    public void shouldOpenApp() throws IOException {
+        WebDriverWait wait = new WebDriverWait(getDriver(), 10000);
+        WebElement loginField = wait.until(ExpectedConditions.visibilityOfElementLocated(MobileBy.AccessibilityId("test-Username")));
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("Screenshot.png"));
+        assertTrue(loginField.isDisplayed());
+    }
+}
